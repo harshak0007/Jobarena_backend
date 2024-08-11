af5cd6eafefc67cd9702b3980110c69c39d05388
Harshak Saini <harshaksaini@gmail.com>
2024-08-11 10:11:02 +0530

    commit

--- config/passport.js
+++ config/passport.js
@@ -14,7 +14,8 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: 'https://jobarena-backend.onrender.com/api/user/google/callback',
+			callbackURL:  'https://jobarena-backend.onrender.com/api/user/google/callback',
+
 		},
 		async (accessToken, refreshToken, profile, done) => {
 			try {
